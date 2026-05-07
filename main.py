60674545be6fbe129616a87542f46f99591d4db0
furnicareus <tisimo@heyzone.cloudns.biz>
2026-05-07 23:01:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeo
 from datetime import datetime
 
 # --- 配置项 ---
-SERVER_URL = "https://panel.godlike.host/server/4b409a61"
+SERVER_URL = "https://panel.godlike.host/server/616fcc4a"
 LOGIN_URL = "https://panel.godlike.host/auth/login"
 COOKIE_NAME = "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d"
 TASK_TIMEOUT_SECONDS = 300  # 5分钟
